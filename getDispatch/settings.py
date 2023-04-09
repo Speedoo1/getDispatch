2af9eb290a162bfe5470236d03f3973690021aba
Speedoo1 <58296620+Speedoo1@users.noreply.github.com>
2023-04-09 08:09:10 +0100

    data base updates

--- getDispatch/settings.py
+++ getDispatch/settings.py
@@ -75,10 +75,22 @@ WSGI_APPLICATION = 'getDispatch.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': '9ox5ipylt0kGswWqjA5w',
+        'HOST': 'containers-us-west-201.railway.app',
+        'PORT': '7334'
+
+
     }
+
 }
 
 # Password validation
